84e27b74302a5c71d008f7c848052a5303045bef
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-04 21:02:15 -0300

    first commit

---
+++ test/cadastro_cliente_test.py
@@ -0,0 +1,20 @@
+import re 
+import sys 
+import random
+from faker import Faker
+sys.path.append('./../')
+from Cadastro.src.cadastro import Cliente, CadastroCliente
+
+faker = Faker()
+
+# Teste unitário
+def test_de_cadastro_de_cliente():
+    #Arrange
+    cliente = Cliente("Allison", 18, "Allison@gamil.com")
+    cadastro_cliente = CadastroCliente()
+    
+    #Act
+    resposta = cadastro_cliente.cadastrar_cliente(cliente)
+    
+    #Assert
+    assert "Cadastrado com sucesso" == resposta
